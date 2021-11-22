5d63321533c34d634e8b9e4e70638a3e8986b713
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-11-22 20:01:48 +0530

    Initial commit

---
+++ src/main/java/com/github/ott/gateway/web/OttGatewayWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.ott.gateway.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttGatewayWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttGatewayWebApplication.class, args);
+	}
+
+}
