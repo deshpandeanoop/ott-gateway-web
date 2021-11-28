5f6b0b61890067fe2fcafeac62cf5dc38e9da040
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-11-28 17:17:05 +0530

    Added GetOTT service details integration

--- src/main/resources/static/js/display-ott-details.js
+++ src/main/resources/static/js/display-ott-details.js
@@ -1,10 +1,21 @@
 function launchOttPlatform(platformType) {
     var ottPlatformUserId = sessionStorage.getItem("ottPlatformUserId");
-    window.alert(ottPlatformUserId);
 
-    $.get("http://localhost:9000/ott-gateway-service/v1/ottaccounts?gatewayUserId="+gatewayUserId+"&fetchOnlyNames=false&platformType=AMAZON_PRIME",
-    function(data, status) {
-        window.alert(data);
-        window.alert(status);
-    });
+    $.get("http://localhost:9000/ott-gateway-service/v1/ottaccounts?gatewayUserId="+ottPlatformUserId+"&fetchOnlyNames=false&platformType="+platformType,
+        function(data, status) {
+            if(status == 'success') {
+               username = data.ottServiceAccounts[0].username;
+               password = data.ottServiceAccounts[0].password;
+               localStorage.setItem("ottUserId", username);
+               localStorage.setItem("ottPassword", password);
+
+               if(platformType == 'AMAZON_PRIME') {
+                var amazonWindow = window.open('https://www.amazon.com/ap/signin?accountStatusPolicy=P1&clientContext=261-8397475-8223164&language=en_US&openid.assoc_handle=amzn_prime_video_desktop_us&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.primevideo.com%2Fauth%2Freturn%2Fref%3Dav_auth_ap%3F_encoding%3DUTF8%26location%3D%252Fref%253Ddv_auth_ret');
+               } else if(platformType == 'NETFLIX') {
+                window.open('https://www.netflix.com/in/login');
+               }
+            } else {
+                window.alert('Cannot fetch user\'s OTT details required for auto launch');
+            }
+     });
 }
